831b556c3c72a5ef783f9bd1d968791b2d60aa26
Myownjormungand@gmail.com <Myownjormungand@gmail.com>
2021-10-22 11:26:42 +0500

    getArticles & getSearchMethodForArticles

---
+++ AdministrationServices/Admin/Controllers/ArticleController.cs
@@ -0,0 +1,67 @@
+﻿using Admin.ApiModels.Request;
+using Admin.ApiModels.Response;
+using Admin.Core;
+using Admin.Models;
+using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Admin.Controllers
+{
+    [ApiController]
+    [Route("admin/[controller]")]
+    public class ArticleController : ControllerBase
+    {
+        private readonly ILogger<ArticleController> _logger;
+        private readonly ApplicationContext _context;
+        private IMapper _mapper;
+
+        public ArticleController(ILogger<ArticleController> logger, ApplicationContext context, IMapper mapper)
+        {
+            _logger = logger;
+            _context = context;
+            _mapper = mapper;
+        }
+        [HttpPost("getArticles")]
+        public async Task<IActionResult> getArticles(ArticleRequest request)
+        {
+            var result = new ArticleResponse();
+
+            var article = await _context.Article.Skip(request.Skip).Take(request.Quantity).Select(p => new Article { ArticleId = p.ArticleId, Title = p.Title }).ToListAsync();
+            if (article.Count == 0)
+            {
+                result.Code = -100;
+                result.Message = "Can't get products with given parameters.";
+                return Ok(result);
+            }
+
+            result.Code = 100;
+            result.Message = "Success";
+            result.Articles = article;
+            return Ok(result);
+        }
+        [HttpPost("getSearchMethodForArticles")]
+        public async Task<IActionResult> GetSearchMethodForArticles(string Name, int Quantity)
+        {
+            var result = new ArticleResponse();
+
+            var article = await _context.Article.Take(Quantity).Where(c => c.Title.StartsWith(Name) || c.Title.Contains(Name) || c.Title.EndsWith(Name)).Select(p => new Article { ArticleId = p.ArticleId, Title = p.Title }).ToListAsync();
+            if (article.Count == 0)
+            {
+                result.Code = -100;
+                result.Message = "Can't get products with given parameters.";
+                return Ok(result);
+            }
+
+            result.Code = 100;
+            result.Message = "Success";
+            result.Articles = article;
+            return Ok(result);
+        }
+    }
+}
---
+++ AdministrationServices/Admin/Models/Article.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace Admin.Models
+{
+    public class Article
+    {
+        [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingDefault)]
+        public Guid ArticleId { get; set; }
+
+        [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingDefault)]
+        public string Title { get; set; }
+
+        [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingDefault)]
+        public string ShortDescription { get; set; }
+
+        [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingDefault)]
+        public string Description { get; set; }
+
+        [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingDefault)]
+        public string ReadTime { get; set; }
+
+        [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingDefault)]
+        public string Tags { get; set; }
+
+        [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingDefault)]
+        public Guid? ProductGameId { get; set; }
+
+        [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingDefault)]
+        public Guid? ArticleSeoId { get; set; }
+
+        [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingDefault)]
+        public string ImagePath { get; set; }
+
+        [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingDefault)]
+        public bool Enabled { get; set; }
+
+        [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingDefault)]
+        public int? Rating { get; set; }
+
+        [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingDefault)]
+        public DateTime? ArticleCreateTime { get; set; }
+
+        [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingDefault)]
+        public DateTime? ArticleUpdateTime { get; set; }
+
+        [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingDefault)]
+        public DateTime ArticlePostTime { get; set; }
+    }
+}
