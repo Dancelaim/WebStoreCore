e193db2b38a8d74073dc7d62ae3db968aa102b60
Myownjormungand@gmail.com <Myownjormungand@gmail.com>
2021-10-22 15:53:42 +0500

    getRank & getSearchMethodForRank

---
+++ AdministrationServices/Admin/Controllers/RankController.cs
@@ -0,0 +1,67 @@
+﻿using Admin.ApiModels.Request;
+using Admin.ApiModels.Response;
+using Admin.Core;
+using Admin.Models;
+using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Admin.Controllers
+{
+    [ApiController]
+    [Route("admin/[controller]")]
+    public class RankController : ControllerBase
+    {
+        private readonly ILogger<RankController> _logger;
+        private readonly ApplicationContext _context;
+        private IMapper _mapper;
+
+        public RankController(ILogger<RankController> logger, ApplicationContext context, IMapper mapper)
+        {
+            _logger = logger;
+            _context = context;
+            _mapper = mapper;
+        }
+        [HttpPost("getRank")]
+        public async Task<IActionResult> GetRank(RankRequest request)
+        {
+            var result = new RankResponse();
+
+            var rank = await _context.Ranks.Skip(request.Skip).Take(request.Quantity).Select(p => new Ranks { RankId = p.RankId, Name = p.Name }).ToListAsync();
+            if (rank.Count == 0)
+            {
+                result.Code = -100;
+                result.Message = "Can't get products with given parameters.";
+                return Ok(result);
+            }
+
+            result.Code = 100;
+            result.Message = "Success";
+            result.Ranks = rank;
+            return Ok(result);
+        }
+        [HttpPost("getSearchMethodForRank")]
+        public async Task<IActionResult> GetSearchMethodForRank(RankRequest request, string Name)
+        {
+            var result = new RankResponse();
+
+            var rank = await _context.Ranks.Take(request.Quantity).Where(c => c.Name.StartsWith(Name) || c.Name.Contains(Name) || c.Name.EndsWith(Name)).Select(p => new Ranks { RankId = p.RankId, Name = p.Name }).ToListAsync();
+            if (rank.Count == 0)
+            {
+                result.Code = -100;
+                result.Message = "Can't get products with given parameters.";
+                return Ok(result);
+            }
+
+            result.Code = 100;
+            result.Message = "Success";
+            result.Ranks = rank;
+            return Ok(result);
+        }
+    }
+}
---
+++ AdministrationServices/Admin/Models/Ranks.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace Admin.Models
+{
+    public class Ranks
+    {
+        [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingDefault)]
+        public Guid RankId { get; set; }
+
+        [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingDefault)]
+        public string Name { get; set; }
+
+        [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingDefault)]
+        public string Sale { get; set; }
+    }
+}
