cdb57293dcee22e9b0c42791d681ddb8bfb8757d
Myownjormungand@gmail.com <Myownjormungand@gmail.com>
2022-05-20 17:52:34 +0300

    fix Seo Model API

--- AdministrationServices/Admin/Models/Seo.cs
+++ AdministrationServices/Admin/Models/Seo.cs
@@ -12,5 +12,15 @@ namespace Admin.Models
         public string MetaTagTitle { get; set; }
         [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingDefault)]
         public Guid SeoId { get; set; }
+        public string MetaTagDescription { get; set; }
+        public string MetaTagKeyWords { get; set; }
+        public string Seotags { get; set; }
+        public string CustomTitle1 { get; set; }
+        public string CustomTitle2 { get; set; }
+        public string CustomImageTitle { get; set; }
+        public string CustomImageAlt { get; set; }
+        public string MetaRobots { get; set; }
+        public string UrlKeyWord { get; set; }
+        public string Seoimage { get; set; }
     }
 }
