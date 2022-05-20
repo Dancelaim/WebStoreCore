2191d8ee0bc13023185295c6d30a8497d9940fcd
Myownjormungand@gmail.com <Myownjormungand@gmail.com>
2022-05-20 18:29:45 +0300

    fix Seo model #2

--- AdministrationServices/Admin/Models/Seo.cs
+++ AdministrationServices/Admin/Models/Seo.cs
@@ -10,17 +10,38 @@ namespace Admin.Models
     {
         [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingDefault)]
         public string MetaTagTitle { get; set; }
+
         [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingDefault)]
         public Guid SeoId { get; set; }
+
+        [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingDefault)]
         public string MetaTagDescription { get; set; }
+
+        [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingDefault)]
         public string MetaTagKeyWords { get; set; }
+
+        [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingDefault)]
         public string Seotags { get; set; }
+
+        [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingDefault)]
         public string CustomTitle1 { get; set; }
+
+        [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingDefault)]
         public string CustomTitle2 { get; set; }
+
+        [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingDefault)]
         public string CustomImageTitle { get; set; }
+
+        [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingDefault)]
         public string CustomImageAlt { get; set; }
+
+        [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingDefault)]
         public string MetaRobots { get; set; }
+
+        [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingDefault)]
         public string UrlKeyWord { get; set; }
+
+        [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingDefault)]
         public string Seoimage { get; set; }
     }
 }
